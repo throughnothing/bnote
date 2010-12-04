39aafce56e1405aa34babdfb293802367b68a0ba
William <throughnothing@gmail.com>
2010-12-04 00:00:55 -0500

    initial changeover to python

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-bNote aims to be a linux clone of Notational Velocity for OS X
+bNote aims to be a linux clone of Notational Velocity
---
+++ main.py
@@ -0,0 +1,115 @@
+import pygtk,gtk,os
+import datetime
+from sqliteds import BNoteDataStore,Note
+
+class BNote:
+  def __init__(self):
+    # Initialize datastore (sqlite for now)
+    self.notes = BNoteDataStore()
+    self.cur_note = None
+
+    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    self.window.set_default_size(500,600)
+
+    # Search Text Box
+    self.search_box = gtk.Entry()  
+    self._setup_search_box_signals()
+
+
+    # Main TextView for notes
+    self.note_text = gtk.TextView()
+    self.note_buffer = self.note_text.get_buffer();
+    self.note_text.editable = True;
+    self.note_text.cursor_visible = True;
+    self._setup_note_signals()
+
+    self._setup_note_list()
+
+    # Scroll For Note Body
+    self.note_scroll = gtk.ScrolledWindow()
+    self.note_scroll.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+    self.note_scroll.add(self.note_text)
+    
+    # Scroll For Note List
+    self.note_list_scroll = gtk.ScrolledWindow()
+    self.note_list_scroll.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+    self.note_list_scroll.add(self.note_list_view)
+
+
+    # Resizable VPaned
+    vpaned = gtk.VPaned()
+    vpaned.add1(self.note_list_scroll)
+    vpaned.add2(self.note_scroll)
+    # Set the default position of the resizer
+    vpaned.set_position(160)
+
+    # VBox for search box + VPaned
+    vbox = gtk.VBox(False,0)
+    vbox.pack_start(self.search_box,False,True,0) 
+    vbox.pack_start(vpaned,True,True,0) 
+
+
+    self.note_scroll.show()
+    self.note_list_scroll.show()
+    vpaned.show()
+    self.note_text.show()
+    self.note_list_view.show()
+    self.search_box.show()
+    vbox.show()
+    self.window.add(vbox)
+    self.window.show()
+
+  def _setup_note_list(self):
+    self.note_list_store = gtk.TreeStore(str,str,str)
+    #Test the TreeView
+    #for i in range(10):
+      #self.note_list_store.append(None, ["blah blah %i" % i, "Today at 10PM", "Today at 11PM"])
+
+    self.note_list_view = gtk.TreeView(self.note_list_store)
+    self.title_column = gtk.TreeViewColumn('Title')
+    self.date_created_column = gtk.TreeViewColumn('Date Created')
+    self.date_modified_column = gtk.TreeViewColumn('Date Modified')
+    self.note_list_view.append_column(self.title_column)
+    self.note_list_view.append_column(self.date_created_column)
+    self.note_list_view.append_column(self.date_modified_column)
+
+    self.cell = gtk.CellRendererText()
+    self.title_column.pack_start(self.cell,True)
+    self.title_column.add_attribute(self.cell,'text',0)
+    self.date_modified_column.pack_start(self.cell,True)
+    self.date_modified_column.add_attribute(self.cell,'text',0)
+    self.date_created_column.pack_start(self.cell,True)
+    self.date_created_column.add_attribute(self.cell,'text',0)
+
+  def _setup_search_box_signals(self):
+    self.search_box.connect("changed",self.search_modified)
+    self.search_box.connect("activate",self.search_activated)
+
+  def search_modified(self,widget,delete_type=None,delete_count=0):
+    text = self.search_box.get_text()
+    if text != "":
+      self.notes.search(text)
+
+  def search_activated(self,widget):
+    text = self.search_box.get_text()
+    if text != "":
+      self.cur_note = self.notes.create(text)
+      self.note_text.grab_focus()
+
+  def _setup_note_signals(self):
+    self.note_buffer.connect("changed",self.note_modified)
+
+  def note_modified(self,widget,delete_type=None,delete_count=0):
+    s_iter = self.note_buffer.get_start_iter()
+    e_iter = self.note_buffer.get_end_iter()
+    text = self.note_buffer.get_text(s_iter,e_iter)
+    self.cur_note.set_text(text)
+    self.notes.save(self.cur_note)
+
+  def main(self):
+    gtk.main()
+
+
+if __name__ == "__main__":
+  bnote = BNote()
+  bnote.main()
